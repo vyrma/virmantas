23d6d3644364bd5fceff0301cbeba2dd6ca984fc
Virmantas <virmantas7@gmail.com>
2022-01-23 01:29:55 +0200

    pirmoji versija

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+
+        body{
+            background-color: #333737;
+        }
+        div.title{
+            position: fixed;
+            margin: 0;
+            padding: 0;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%,-50%);
+        }
+        h1{
+            font-family:sans-serif;
+            color:#9a9fa1
+        }
+    </style>
+</head>
+<body>
+    <div class="title">
+        <h1>Kuriama...</h1>
+    </div>
+</body>
+</html>
