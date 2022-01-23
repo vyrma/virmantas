c69a9b2cff627345c43c09e5145a8f11d3fe5bc6
Virmantas <virmantas7@gmail.com>
2022-01-24 01:46:23 +0200

    mokomes css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,28 +4,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-
-        body{
-            background-color: #333737;
-        }
-        div.title{
-            position: fixed;
-            margin: 0;
-            padding: 0;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%,-50%);
-        }
-        h1{
-            font-family:sans-serif;
-            color:#9a9fa1
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
-    <div class="title">
-        <h1>Kuriama...</h1>
-    </div>
+
+    <header class="hero">
+        <div class="container spacing">
+            <h1 class="primary-title">Virmantas Portfolio</h1>
+            <p>
+                Simi is everything.
+            </p>
+            <a href="#" class="btn"></a>
+            </div>  
+    </header>
+
+    <main>
+        <section class="featured">
+           <div class="contariner">
+               <h2 class="section-title">Featured Products</h2>
+                <div class="split">
+                    <a href="" class="featured_item">
+                        <img src="{{ asset('programming_logo.png') }}" alt="feature_img">
+                        <p class="featured_details"><span class="price">test</span>TArkim</p>
+                    </a>
+                    <a href="" class="featured_item">
+                        <img src="{{ asset('programming_logo.png') }}" alt="feature_img">
+                        <p class="featured_details"><span class="price">test</span>TArkim</p>
+                    </a>
+                    <a href="" class="featured_item">
+                        <img src="{{ asset('programming_logo.png') }}" alt="feature_img">
+                        <p class="featured_details"><span class="price">test</span>TArkim</p>
+                    </a>
+                    </div>
+                </div> 
+        </section>
+        <section class="our-products">
+            <div class="container"></div>
+            <h2 class="section-title">Our Products</h2>
+            <article class="product pro-red spacing">
+                <img src="{{ asset('programming_logo.png') }}" alt="" class="product_image">
+                <h3 class="product_title"></h3>
+                <div class="production_description"></div>
+                <a href="" class="btn">Spausk</a>
+            </article>
+
+            <article class="product pro-white spacing">
+                <img src="{{ asset('programming_logo.png') }}" alt="" class="product_image">
+                <h3 class="product_title"></h3>
+                <div class="production_description"></div>
+                <a href="" class="btn">Spausk</a>
+            </article>
+
+            <article class="product pro-blue spacing">
+                <img src="{{ asset('programming_logo.png') }}" alt="" class="product_image">
+                <h3 class="product_title"></h3>
+                <div class="production_description"></div>
+                <a href="" class="btn">Spausk</a>
+            </article>
+        </section>
+    </main>
 </body>
 </html>
